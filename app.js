74649b5db56863b5a6e671d8492582fd2db812e5
Trachea <xtrachea@gmail.com>
2022-01-10 17:22:28 -0500

    Finished minimal viable project :)

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ const musicItems = [
         "releaseMonth": "12",
         "releaseYear": "2021",
         "runtime": "2:40",
-        "type": "single"
+        "type": "single",
+        "stream": "https://open.spotify.com/album/2CnKE4acournZtJ9Qh9vp4?si=TqZnYRldQuqhl_biz6mFkw"
     },
     {
         "id": "golden-hour",
@@ -22,7 +23,8 @@ const musicItems = [
         "releaseMonth": "8",
         "releaseYear": "2020",
         "runtime": "20:13",
-        "type": "project"
+        "type": "project",
+        "stream": "https://open.spotify.com/album/7ykrCsXPbp1DqM71Ct5kpc?si=oj6y1i7EQ1O2xoZbFEWYGQ"
     },
     {
         "id": "isolate",
@@ -30,7 +32,8 @@ const musicItems = [
         "releaseMonth": "11",
         "releaseYear": "2019",
         "runtime": "24:31",
-        "type": "project"
+        "type": "project",
+        "stream": "https://open.spotify.com/album/5dyHSePYz5Rsniy5Cyl8fl?si=6CNmwcH6RnCSxFOyIs7RPA"
     },
     {
         "id": "sakura",
@@ -38,7 +41,8 @@ const musicItems = [
         "releaseMonth": "4",
         "releaseYear": "2019",
         "runtime": "14:59",
-        "type": "project"
+        "type": "project",
+        "stream": ""
     },
     {
         "id": "pink-november-2",
@@ -46,7 +50,8 @@ const musicItems = [
         "releaseMonth": "11",
         "releaseYear": "2018",
         "runtime": "23:47",
-        "type": "project"
+        "type": "project",
+        "stream": "https://open.spotify.com/album/6Mu80yTLoEQH7IpYrPw7h9?si=fxSYVMiWQxanGZOtUypBfA"
     },
     {
         "id": "gambit",
@@ -54,7 +59,8 @@ const musicItems = [
         "releaseMonth": "6",
         "releaseYear": "2018",
         "runtime": "28:18",
-        "type": "project"
+        "type": "project",
+        "stream": "https://open.spotify.com/album/3bPDNHYCig60TMxG1vIIFJ?si=EadXQXckQYWa4GqC7l-PBA"
     },
     {
         "id": "pink-november",
@@ -62,7 +68,8 @@ const musicItems = [
         "releaseMonth": "11",
         "releaseYear": "2017",
         "runtime": "15:24",
-        "type": "project"
+        "type": "project",
+        "stream": ""
     },
     {
         "id": "cyclical",
@@ -70,7 +77,8 @@ const musicItems = [
         "releaseMonth": "6",
         "releaseYear": "2017",
         "runtime": "22:36",
-        "type": "project"
+        "type": "project",
+        "stream": "https://www.youtube.com/watch?v=Xsg-iYj5rQ4&ab_channel=Gothreau"
     }
 
 ]
@@ -107,13 +115,13 @@ musicItems.forEach(function (project) {
                 </div>  
                 <div class="music-item-back">
                     <img src="assets/artworks/` + project.id + `-cover.jpg" style="width:100%; height:100%">
-                    <div class="music-item-back-info">
+                    <div class="music-item-back-title"> 
                         <h1>` + project.name + `</h1>
-                        <h2>` + months.get(project.releaseMonth).substring(0, 3) + ` ` + project.releaseYear + `</h2>
-                        <div class="button_stream button">
-                            <a href="` + project.stream + `" target="_blank">stream now</a>
-                        </div>
-                    </div>
+                    <div>
+                    <h2>` + months.get(project.releaseMonth).substring(0, 3) + ` ` + project.releaseYear + `</h2>
+                    <div class="button_stream button">
+                        <a href="` + project.stream + `" target="_blank">stream now</a>
+                    </div>  
                 </div>
             </div>
         </div>
@@ -127,12 +135,15 @@ musicItems.forEach(function (project) {
                 </div>  
                 <div class="music-item-back">
                     <img src="assets/artworks/` + project.id + `-cover.jpg" style="width:100%; height:100%">
-                    <div class="music-item-back-info">
+                    <div class="music-item-back-title"> 
                         <h1>` + project.name + `</h1>
-                        <h2>` + months.get(project.releaseMonth).substring(0, 3) + ` ` + project.releaseYear + `</h2>
-                        <div class="button_stream button">
-                            <a href="` + project.stream + `" target="_blank">stream now</a>
-                        </div>
+                    <div>
+                    <h2>` + months.get(project.releaseMonth).substring(0, 3) + ` ` + project.releaseYear + `</h2>
+                    <div class="button_stream button">
+                        <a href="` + project.stream + `" target="_blank">stream now</a>
+                    </div>
+                    <div class="button_info button">
+                        <a href="` + project.stream + `" target="_blank">MORE INFO</a>
                     </div>
                 </div>
             </div>
