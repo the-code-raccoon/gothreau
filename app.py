3fe2ed96cae3f1cc4070b664c56a4e8a199ebeeb
Trachea <xtrachea@gmail.com>
2022-01-21 10:48:47 -0500

    added html template inhertience

--- app.py
+++ app.py
@@ -1,58 +1,25 @@
-from flask import Flask, request, jsonify
+from math import degrees
+from flask import Flask, render_template
+from markupsafe import escape
+
 app = Flask(__name__)
 
-@app.route('/getmsg/', methods=['GET'])
-def respond():
-    # Retrieve the name from url parameter
-    name = request.args.get("name", None)
-
-    # For debugging
-    print(f"got name {name}")
-
-    response = {}
-
-    # Check if user sent a name at all
-    if not name:
-        response["ERROR"] = "no name found, please send a name."
-    # Check if the user entered a number not a name
-    elif str(name).isdigit():
-        response["ERROR"] = "name can't be numeric."
-    # Now the user entered a valid name
-    else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
-
-    # Return the response in json format
-    return jsonify(response)
-
-@app.route('/post/', methods=['POST'])
-def post_something():
-    param = request.form.get('name')
-    print(param)
-    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-    if param:
-        return jsonify({
-            "Message": f"Welcome {name} to our awesome platform!!",
-            # Add this option to distinct the POST request
-            "METHOD" : "POST"
-        })
-    else:
-        return jsonify({
-            "ERROR": "no name found, please send a name."
-        })
-
-# A welcome message to test our server
-@app.route('/')
+@app.route("/")
 def index():
-    return "<h1>Welcome to our server !!</h1>"
+    # return render_template('index.html', page_name="Gothreau")
+    return render_template('index.html')
 
-if __name__ == '__main__':
-    # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
+@app.route("/music")
+def music():
+    return render_template('music.html')
 
-# from flask import Flask
+@app.route("/about")
+def about():
+    return render_template('about.html')
 
-# app = Flask(__name__)
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
 
-# @app.route("/")
-# def index():
-#     return "ayo"
+if __name__ == "__main__":
+    app.run(debug=True)
